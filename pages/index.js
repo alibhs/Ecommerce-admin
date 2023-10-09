8d1adbc1f52ad25263648fe59e678fa10858e927
alibhs <alibahsi772@gmail.com>
2023-10-09 15:43:15 +0300

    order sayfası tamamlandı

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout";
 export default function Home() {
 return <Layout>
   <div className="text-blue-900">
-    Hoşgeldin, Ali
+    Hoşgeldiniz
   </div> 
 </Layout>
 
